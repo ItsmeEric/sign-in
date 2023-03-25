6171350d4598260da8d840b5365a82d7166bee98
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-03-25 22:59:52 +0200

    Add toggle function using ternary operator

--- src/script.js
+++ src/script.js
@@ -2,4 +2,9 @@ function togglePassword() {
   document
     .querySelectorAll(".eye")
     .forEach((eye) => eye.classList.toggle("hide"));
+
+  const type =
+    password.getAttribute("type") == "password" ? "type" : "password";
+
+  password.setAttribute("type", type);
 }
